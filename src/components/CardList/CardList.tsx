b828373b61218c7f7b783b7b2f3f42a407cb5e43
Igor Manakhau <mi8mto@gmail.com>
2026-05-17 11:24:24 +0300

    refactor: convert CardList to function component

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,30 +1,23 @@
-import { Component } from 'react';
 import type { Pokemon } from '../../types/api';
 
 interface CardListProps {
   pokemonList: Pokemon[];
 }
 
-export class CardList extends Component<CardListProps> {
-  private getPokemonId(url: string): string {
-    const segments = url.replace(/\/$/, '').split('/');
-    return segments[segments.length - 1];
-  }
+const getPokemonId = (url: string): string => {
+  const segments = url.replace(/\/$/, '').split('/');
+  return segments[segments.length - 1];
+};
 
-  render() {
-    const { pokemonList } = this.props;
-
-    return (
-      <>
-        {pokemonList.map((pokemon) => (
-          <div key={pokemon.name} className="card">
-            <h3>{pokemon.name}</h3>
-            <p className="card-description">
-              Pokemon #{this.getPokemonId(pokemon.url)}
-            </p>
-          </div>
-        ))}
-      </>
-    );
-  }
-}
+export const CardList = ({ pokemonList }: CardListProps) => {
+  return (
+    <>
+      {pokemonList.map((pokemon) => (
+        <div key={pokemon.name} className="card">
+          <h3>{pokemon.name}</h3>
+          <p className="card-description">Pokemon #{getPokemonId(pokemon.url)}</p>
+        </div>
+      ))}
+    </>
+  );
+};
