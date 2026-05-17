81eb0c8d527b1c6501295e9f04b435729c96fc08
Igor Manakhau <mi8mto@gmail.com>
2026-05-17 20:20:35 +0300

    feat: make cards selectable

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -2,6 +2,7 @@ import type { Pokemon } from '../../types/api';
 
 interface CardListProps {
   pokemonList: Pokemon[];
+  onPokemonSelect?: (pokemonId: string) => void;
 }
 
 const getPokemonId = (url: string): string => {
@@ -9,17 +10,24 @@ const getPokemonId = (url: string): string => {
   return segments[segments.length - 1];
 };
 
-export const CardList = ({ pokemonList }: CardListProps) => {
+export const CardList = ({ pokemonList, onPokemonSelect }: CardListProps) => {
   return (
     <>
-      {pokemonList.map((pokemon) => (
-        <div key={pokemon.name} className="card">
-          <h3>{pokemon.name}</h3>
-          <p className="card-description">
-            Pokemon #{getPokemonId(pokemon.url)}
-          </p>
-        </div>
-      ))}
+      {pokemonList.map((pokemon) => {
+        const pokemonId = getPokemonId(pokemon.url);
+
+        return (
+          <button
+            key={pokemon.name}
+            type="button"
+            className="card"
+            onClick={() => onPokemonSelect?.(pokemonId)}
+          >
+            <h3>{pokemon.name}</h3>
+            <p className="card-description">Pokemon #{pokemonId}</p>
+          </button>
+        );
+      })}
     </>
   );
 };
