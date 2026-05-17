27e85db774ef478b776628df2117bac0dcef7793
Igor Manakhau <mi8mto@gmail.com>
2026-05-18 00:29:20 +0300

    feat: improve not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export const NotFoundPage = () => {
   return (
-    <main>
+    <main className="not-found-page">
       <h1>404</h1>
       <p>Page not found</p>
       <Link to="/?page=1">Back to main page</Link>
