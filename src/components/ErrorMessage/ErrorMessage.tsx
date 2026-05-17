107018f8230315e27cb2ebf7da787ae0e98825e6
Igor Manakhau <mi8mto@gmail.com>
2026-05-17 11:05:10 +0300

    refactor: convert ErrorMessage to function component

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,16 +1,13 @@
-import { Component } from 'react';
 import './ErrorMessage.css';
 
 interface ErrorMessageProps {
   message: string;
 }
 
-export class ErrorMessage extends Component<ErrorMessageProps> {
-  render() {
-    return (
-      <div className="error-message">
-        <p>{this.props.message}</p>
-      </div>
-    );
-  }
-}
+export const ErrorMessage = ({ message }: ErrorMessageProps) => {
+  return (
+    <div className="error-message">
+      <p>{message}</p>
+    </div>
+  );
+};
