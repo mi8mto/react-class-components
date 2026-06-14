21252362d6369756f688911c2cea2487aafab853
Igor Manakhau <mi8mto@gmail.com>
2026-06-14 18:38:42 +0300

    perf: memoize country card component

--- src/components/country-card/country-card.tsx
+++ src/components/country-card/country-card.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import type { Country } from '../../types';
 import { DataTable } from '../data-table/data-table';
 import {
@@ -15,7 +16,7 @@ type CountryCardProps = {
   selectedColumns: string[];
 };
 
-export const CountryCard = ({ country, selectedYear, selectedColumns }: CountryCardProps) => {
+const CountryCardComponent = ({ country, selectedYear, selectedColumns }: CountryCardProps) => {
   const yearDataMap = createYearDataMap(country.data);
   const population = getPopulationForYear(yearDataMap, selectedYear);
   const co2 = getCo2ForYear(yearDataMap, selectedYear);
@@ -40,3 +41,5 @@ export const CountryCard = ({ country, selectedYear, selectedColumns }: CountryC
     </div>
   );
 };
+
+export const CountryCard = memo(CountryCardComponent);
