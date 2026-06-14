feecf1132598e0f25f7838808e034bd98ac835be
Igor Manakhau <mi8mto@gmail.com>
2026-06-14 19:26:06 +0300

    perf: memoize data table component

--- src/components/data-table/data-table.tsx
+++ src/components/data-table/data-table.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import type { YearData } from '../../types';
 import { formatNumber } from '../../utils/format-utils';
 
@@ -9,7 +10,7 @@ type DataTableProps = {
   columns: string[];
 };
 
-export const DataTable = ({ data, year, columns }: DataTableProps) => {
+const DataTableComponent = ({ data, year, columns }: DataTableProps) => {
   const yearData = data.filter((d) => d.year === year);
 
   if (yearData.length === 0) {
@@ -35,3 +36,5 @@ export const DataTable = ({ data, year, columns }: DataTableProps) => {
     </table>
   );
 };
+
+export const DataTable = memo(DataTableComponent);
